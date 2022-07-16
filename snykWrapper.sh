ecc68a4d8e401105aab0a679eaf0ce83fcedc04f
Peter Chen <z5255813@ad.unsw.edu.au>
2022-07-16 21:30:20 +1000

    Working bash wrapper for the Snyk service

--- snykWrapper.sh
+++ snykWrapper.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script is a wrapper for calling snyk against a target directory
 
-snyk test
+prefix="${1:-SnykOutput_}"         
+number=1                           
+ext="json"                         
+
+printf -v num "%03d" "$number"      
+fname="$prefix$num.$ext"            
+
+while [ -e "SnykOutput/$fname" ]; do 
+  echo "abc"
+  number=$((number + 1))            
+  printf -v num "%03d" "$number"    
+  fname="$prefix$num.$ext"          
+done
+
+snyk test --json > "SnykOutput/$fname"
